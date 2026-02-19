f0a9973a1d9b53427494a6207f9ceaaab60fd56d
AariyJP <aariyjp@aariy.net>
2026-02-19 11:42:09 +0900

    refactor: sync upstream

--- src/views/Settings/dialogs/ChangelogDialog.vue
+++ src/views/Settings/dialogs/ChangelogDialog.vue
@@ -31,7 +31,7 @@
                 <Button
                     variant="ghost"
                     class="mr-2"
-                    @click="openExternalLink('https://github.com/AariyJP/VRCXF/releases')">
+                    @click="openExternalLink(`${links.github}/releases`)">
                     {{ t('dialog.change_log.github') }}
                 </Button>
                 <!-- <Button variant="outline" class="mr-2" @click="openExternalLink('https://patreon.com/Natsumi_VRCX')">
@@ -52,6 +52,7 @@
     import { storeToRefs } from 'pinia';
     import { useI18n } from 'vue-i18n';
 
+    import { links } from '../../../shared/constants';
     import { openExternalLink } from '../../../shared/utils';
     import { useVRCXUpdaterStore } from '../../../stores';
 
