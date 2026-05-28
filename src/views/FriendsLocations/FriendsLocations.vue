b5ab95046dc05248a26701f2711fa5d9c5735e63
AariyJP <aariyjp@aariy.net>
2026-05-28 22:44:57 +0900

    fix(friends-locations): prevent instance header overflow in narrow widths

--- src/views/FriendsLocations/FriendsLocations.vue
+++ src/views/FriendsLocations/FriendsLocations.vue
@@ -92,7 +92,7 @@
                     <div class="friend-view__virtual-row" :class="`friend-view__virtual-row--${row.type}`">
                         <template v-if="row.type === 'header'">
                             <header class="friend-view__instance-header">
-                                <Location class="text-xs" :location="getRowInstanceId(row)" />
+                                <Location class="text-xs w-full min-w-0" :location="getRowInstanceId(row)" />
                                 <InstanceActionBar
                                     class="text-sm"
                                     :location="getRowInstanceId(row)"
@@ -1081,7 +1081,8 @@
         flex-direction: column;
         align-items: flex-start;
         gap: 4px;
-        width: fit-content;
+        width: 100%;
+        min-width: 0;
         max-width: 100%;
         padding: 4px 2px;
         font-weight: 600;
