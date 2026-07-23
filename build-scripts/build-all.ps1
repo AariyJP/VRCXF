170a511b33b27a5258aee1ad7a1b1ccbcb5b945c
AariyJP <aariyjp@aariy.net>
2026-07-23 20:45:56 +0900

    build!: disable self-contained runtime, trim libs

--- build-scripts/build-all.ps1
+++ build-scripts/build-all.ps1
@@ -1,5 +1,5 @@
 cd ..
-Set-Content -Path "Version" -Value "Forked-AariyJP"
+Set-Content -Path "Version" -Value "Forked-AariyJP" -NoNewline
 
 $ErrorActionPreference = "Stop"
 
@@ -12,7 +12,7 @@ $ZipName = "VRCX_" + $Date + ".zip"
 $SetupName = "VRCX_" + $Date + "_Setup.exe"
 
 Write-Host "Building .Net..." -ForegroundColor Green
-dotnet build Dotnet\VRCX-Cef.csproj -p:Configuration=Release -p:WarningLevel=0 -p:Platform=x64 -p:RestorePackagesConfig=true -t:"Restore;Clean;Build" -m --self-contained
+dotnet build Dotnet\VRCX-Cef.csproj -p:Configuration=Release -p:WarningLevel=0 -p:Platform=x64 -p:RestorePackagesConfig=true -t:"Restore;Clean;Build" -m
 
 Write-Host "Building Node.js..." -ForegroundColor Green
 Remove-Item -Path "node_modules" -Force -Recurse -ErrorAction SilentlyContinue
